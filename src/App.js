2052f99b3039f6bf6e8e417783b0c79ce22444e6
Sehajpreet Kaur <sehajpreetk2967@gmail.com>
2026-03-28 12:20:58 +0530

    new commit added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react"
+
 
 function App() {
+
+  const [value,setValue]=useState("")
+  const [message,setMessage]=useState(null);
+  const [prevChats,setPrevChats]=useState([]);
+  const [currentTitle,setCurrentTitle]=useState(null);
+
+  const createNewChat=()=>{
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick= (uniqueTitle)=>{
+    setCurrentTitle(uniqueTitle);
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages=async()=>{
+
+    const options={
+      method:"POST",
+      body:JSON.stringify({
+          message:value
+      }),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+
+    try{
+      const response=await fetch('http://localhost:8000/completions',options)
+      const data=await response.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    }catch(error){
+      console.log(error)
+    }
+
+    // setMessage("")
+  }
+  useEffect(()=>{
+    if(!currentTitle && value && message){
+      setCurrentTitle(value)
+    }
+    if(currentTitle && value && message){
+      setPrevChats(prevChats =>(
+        [
+          ...prevChats,{
+            //when we open prevchat , we're assinging title,role and content from prev
+            title:currentTitle,
+            role:"user",
+            content:value
+          },
+          {
+            //for response from Ai which is sent for other object 
+            title:currentTitle,
+            role:message.role,
+            content:message.content
+          }
+        ]
+      ))
+      setValue("")
+    }
+  },[message,currentTitle])
+
+  const currentChat=prevChats.filter(prevChat => prevChat.title === currentTitle )
+
+  const uniqueTitles=Array.from(new Set(prevChats.map(prevChat =>prevChat.title)))
+
+
+  console.log(currentChat)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button onClick={createNewChat}>New Chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle,index) =><li key={index} onClick={()=>handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+        </ul>
+          <nav>
+            <p>Made by Sehaj</p>
+          </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>MyGPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage,index)=> <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e)=> setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>➤</div>
+          </div>
+          <p className="info">
+            Chat GPT 14 version. Free Research Preview. Our Goal is to make AI systems more natural and safe to interact with.Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
